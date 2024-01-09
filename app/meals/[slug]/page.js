7648af8ed5135dd5aa05f845218c1f80969c4bb2
Oscar Harron <oscarharron@hotmail.com>
2024-01-10 09:49:18 +1300

    add dynamic routes

---
+++ app/meals/[slug]/page.js
@@ -0,0 +1,7 @@
+export default function Meals({ params }) {
+  return (
+    <main>
+      <h1>Hi I am a {params.slug}</h1>
+    </main>
+  );
+}
---
+++ app/meals/page.js
@@ -0,0 +1,7 @@
+export default function meals() {
+  return (
+    <main>
+      <h1>See our meals!</h1>
+    </main>
+  );
+}
